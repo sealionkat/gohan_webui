c2331576772ebf4265f66d8e566e4987a97ee1a4
Katarzyna Fokow <sealionkat@gmail.com>
2017-02-02 14:14:44 +0100

    Added hiding element in form.
    Fixed problem with filling form data.
    
    Added showing element.
    
    Changed form data within showing/hiding element.
    
    Added missing import.

--- src/Dialog/Dialog.jsx
+++ src/Dialog/Dialog.jsx
@@ -5,11 +5,11 @@ import Form from 'react-jsonschema-form';
 import widgets from './formComponents/widgets';
 import fields from './formComponents/fields';
 import Template from './formComponents/Template';
+import _ from 'lodash';
 
 import {fetchRelationFields, clearData} from './DialogActions';
 
-export class GeneratedDialog extends Component {
-// class GeneratedDialog extends Component {
+class GeneratedDialog extends Component {
   componentDidMount() {
     this.props.fetchRelationFields(this.props.schema.schema, this.props.action, this.props.schema.parent);
   }
@@ -18,9 +18,73 @@ export class GeneratedDialog extends Component {
     this.props.clearData();
   }
 
+  componentWillReceiveProps(nextProps) {
+    if (this.state === null || this.state.currentSchema === undefined) {
+      const formData = nextProps.dialogReducer.schema.propertiesOrder.reduce(
+        (result, item) => {
+          result[item] = nextProps.data[item];
+          return result;
+        }, {}
+      );
+
+      this.setState({
+        currentSchema: _.cloneDeep(nextProps.dialogReducer.schema),
+        currentPropertiesOrder: _.cloneDeep(nextProps.dialogReducer.schema.propertiesOrder),
+        formData
+      });
+    }
+  }
+
   handleSubmit = ({formData}) => {
     this.props.onSubmit(formData, this.props.data.id);
     this.props.onClose(); // Add check success
+  };
+
+  adjustSchema = ({formData}) => {
+    let {currentSchema, currentPropertiesOrder} = this.state;
+    const originalSchema = this.props.dialogReducer.schema;
+    const logic = {
+      color: {
+        Green: {
+          hide: ['favfood']
+        },
+        Blue: {
+          show: ['favfood']
+        }
+      }
+    };
+
+    for (let item in formData) {
+      if (formData.hasOwnProperty(item)) {
+        const value = formData[item];
+        if (value !== undefined && logic[item] !== undefined && logic[item][value] !== undefined) {
+          const hide = logic[item][value].hide || [];
+          const show = logic[item][value].show || [];
+
+          for (let i = 0; i < hide.length; ++i) {
+            const elementToHide = hide[i];
+            if (currentSchema.properties[elementToHide] === undefined) {
+              continue;
+            }
+            delete currentSchema.properties[elementToHide];
+            currentPropertiesOrder.splice(currentPropertiesOrder.indexOf(elementToHide), 1);
+            delete formData[elementToHide];
+          }
+
+          for (let i = 0; i < show.length; ++i) {
+            const elementToShow = show[i];
+            if (currentSchema.properties[elementToShow] !== undefined) {
+              continue;
+            }
+            currentSchema.properties[elementToShow] = originalSchema.properties[elementToShow];
+            currentPropertiesOrder.splice(originalSchema.propertiesOrder.indexOf(elementToShow), 0, elementToShow);
+            formData[elementToShow] = undefined;
+          }
+        }
+      }
+    }
+
+    this.setState({currentSchema, currentPropertiesOrder, formData});
   };
 
   render() {
@@ -51,17 +115,11 @@ export class GeneratedDialog extends Component {
 
             return (
               <div>
-                <Form ref={c => {this.form = c;}} schema={this.props.dialogReducer.schema}
+                <Form ref={c => {this.form = c;}} schema={this.state.currentSchema}
                   fields={fields} widgets={widgets}
-                  FieldTemplate={Template} formData={
-                    this.props.dialogReducer.schema.propertiesOrder.reduce(
-                      (result, item) => {
-                        result[item] = this.props.data[item];
-                        return result;
-                      }, {}
-                    )}
-                  uiSchema={{'ui:order': this.props.dialogReducer.schema.propertiesOrder}} onSubmit={this.handleSubmit}
-                  showErrorList={false}>
+                  FieldTemplate={Template} formData={this.state.formData}
+                  uiSchema={{'ui:order': this.state.currentPropertiesOrder}} onSubmit={this.handleSubmit}
+                  showErrorList={false} onChange={this.adjustSchema}>
                   <div/>
                 </Form>
               </div>
